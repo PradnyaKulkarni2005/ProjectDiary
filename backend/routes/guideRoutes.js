e4365241f018d1e1daa70a9d56d72c9dba7671d6
PradnyaKulkarni2005 <kulkarnipradnya1245@gmail.com>
2025-09-01 20:52:16 +0530

    added backend for guides to evaluate each reviews

--- backend/routes/guideRoutes.js
+++ backend/routes/guideRoutes.js
@@ -3,12 +3,19 @@ const router = express.Router();
 const {
   getGuidesByStudentUserId,
   getGuideInvites,
-  respondToInvite
+  respondToInvite,
+  addReviewAssessment,
+  updateReviewAssessment,
+  getReviewsByTeam
 } = require('../controllers/guideController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.get('/by-department/:userId', getGuidesByStudentUserId);
-router.get('/invites', protect, getGuideInvites); // ✅ added protect
+router.get('/invites', protect, getGuideInvites); // added protect
 router.post('/respond-invite', protect, respondToInvite); // also protect if needed
+router.get('/reviews/:teamId',  getReviewsByTeam); 
+router.post('/reviews', protect,addReviewAssessment);
+router.put('/reviews/:id',protect, updateReviewAssessment); 
+
 
 module.exports = router;
